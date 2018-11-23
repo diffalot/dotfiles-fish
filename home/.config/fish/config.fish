7e9e4995409b9b36da5e05570294d2d91dedccb2
Alice Davis <alice@gigantic.computer>
2018-11-23 10:42:32 -0800

    add inssh-agent config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,9 @@
 # use nerd font themeing
 set -g theme_nerd_fonts yes
 
+# setup ssh-agent
+set -U keychain_init_args --quiet --agents ssh id_rsa
+
 # homeshick dotfile management
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
