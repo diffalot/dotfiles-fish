90f574de74a7eb3a74d501d7cd23c6b8464c8b3d
Alice Davis <alice@gigantic.computer>
2018-11-23 09:27:36 -0800

    switch from fundle to oh-my-fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,12 @@
+# use nerd font themeing
+set -g theme_nerd_fonts yes
 
+# homeshick dotfile management
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
+
+# aliases
+alias emacs="emacs --no-window-system"
+
+# setup racket path
+set PATH $HOME/build/racket-7.1.0.2/bin $PATH
