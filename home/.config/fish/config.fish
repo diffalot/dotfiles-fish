a12ff9aeeb920faf93c5d4f380f6f22979c7d222
Alice Davis <alice@gigantic.computer>
2018-12-02 13:36:01 -0800

    add command to install default npm packages

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,15 +1,40 @@
+# setup racket path
+set PATH $HOME/build/racket-7.1.0.2/bin $PATH
+
+# setup rbenv path
+set PATH $HOME/.rbenv/bin $PATH
+
 # use nerd font themeing
 set -g theme_nerd_fonts yes
 
 # setup ssh-agent
-set -U keychain_init_args --quiet --agents ssh id_rsa
+set -U keychain_init_args --quiet --agents ssh,gpg id_rsa 0xBC76F379DB7CE625
 
 # homeshick dotfile management
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
 
 # aliases
-alias emacs="emacs --no-window-system"
+alias emacs="emacsclient --tty --create-frame"
+alias vim="nvim"
 
-# setup racket path
-set PATH $HOME/build/racket-7.1.0.2/bin $PATH
+# toolchain
+set -U default_npm_packages \
+    http-server \
+    nodemon \
+    lerna \
+    tern \
+    js-beautify \
+    eslint \
+    eslint-config-standard \
+    eslint-plugin-standard \
+    eslint-plugin-promise \
+    eslint-plugin-import \
+    eslint-plugin-node \
+    neovim
+
+
+function toolchain-node
+    npm i -g $default_npm_packages
+end
+
