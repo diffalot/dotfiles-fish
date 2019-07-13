3c27126df7e76d3716fd27afe65439878235fd82
Alice Davis <alice@medium.com>
2019-07-13 13:55:37 -0700

    medium updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -24,20 +24,42 @@ source "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
 
 if test -e ~/.config/fish/secrets.fish
-    source ~/.config/fish/secrets.fish
+    . ~/.config/fish/secrets.fish
 end
 
+# set EDITOR environment variable
+set -Ux EDITOR nvim
 
 # aliases
 alias emacs="emacsclient --tty --create-frame"
 alias vim="nvim"
+alias wip="git commit -a -m wip"
+alias amend="git commit -a --amend -m (git log --oneline --format=%B -n 1 HEAD | head -n 1)"
+alias undo="git reset --soft HEAD~1 && git reset HEAD ."
+alias oneline="git log --oneline master..."
+
+# Medium environment
+if test -e /opt/medium/env
+    bass source /opt/medium/env
+    set -Ux ALLOW_NO_REVIEWERS 1
+end
+
+# spark path
+if test -e /usr/local/spark/bin
+    set PATH /usr/local/spark/bin $PATH
+end
 
 # toolchain
 set -U default_npm_packages \
     http-server \
+    browser-sync \
+    parcel \
     nodemon \
+    onchange \
     lerna \
     tern \
+    flow-bin \
+    jest \
     js-beautify \
     eslint \
     eslint-config-standard \
@@ -46,14 +68,28 @@ set -U default_npm_packages \
     eslint-plugin-import \
     eslint-plugin-node \
     eslint-plugin-vue \
+    eslint-plugin-mocha \
     neovim \
     yo \
     generator-generator \
     @vue/cli \
-    @vue/cli-service-global
+    @vue/cli-service-global \
+    @ceejbot/tarot
 
 
 function toolchain-node
     npm i -g $default_npm_packages
 end
 
+set -U patronus_servers \
+    lite \
+    rito \
+    medium2
+
+function patronus
+    cd ~/code/mono/lite
+    yarn auth
+    for server in $patronus_servers
+        tmux split-window -p 90 "yarn dev $server; or fish"
+    end
+end
