233189b3fb1cf6c729dba1ad1ee9061f835115f8
Alice Davis <alice@gigantic.computer>
2021-06-18 20:30:53 -0700

    use a 4 space tab

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,7 +2,7 @@
 # add .bin to path
 if contains $HOME/bin $fish_user_paths
 else
-  set -U fish_user_paths $fish_user_paths $HOME/bin
+    set -U fish_user_paths $fish_user_paths $HOME/bin
 end
 
 set -g theme_nerd_fonts yes
@@ -14,87 +14,87 @@ set -U VISUAL nvim
 # FUNCTIONS
 # print out a directory tree without junk
 function root
-  # todo(alice) make this read from git ignore
-  tree -aI '.git|node_modules|.next|.DS_Store' -- $argv
+    # todo(alice) make this read from git ignore
+    tree -aI '.git|node_modules|.next|.DS_Store' -- $argv
 end
 
 # fetch all remotes after changing directories
 # into a repository then print a pretty graph
 function git-fetch-river
-  cd $argv[0]
-  git fetch --all
-  git log-tree $argv
+    cd $argv[0]
+    git fetch --all
+    git log-tree $argv
 end
 
 # homeshick cd into a repo and print the graph
 function homeshick-river 
-  homeshick cd dotfiles-
-  git-fetch-river 
+    homeshick cd dotfiles-
+    git-fetch-river 
 end
 
 function buildAndInstallNeovim
-  cd ~/build/neovim
-  and git checkout master; and git pull;
-  and make distclean; and make CMAKE_BUILD_TYPE=Release;
-  and sudo make install
-  and cd -
+    cd ~/build/neovim
+    and git checkout master; and git pull;
+    and make distclean; and make CMAKE_BUILD_TYPE=Release;
+    and sudo make install
+    and cd -
 end
 
 function buildAndInstallFishShell
-  cd ~/build/fish-shell
-  and git checkout master; and git pull;
-  and mkdir -p build; and cd build;
-  and make clean;
-  and cmake ..
-  and make;
-  and sudo make install
-  and cd -
+    cd ~/build/fish-shell
+    and git checkout master; and git pull;
+    and mkdir -p build; and cd build;
+    and make clean;
+    and cmake ..
+    and make;
+    and sudo make install
+    and cd -
 end
 # git stash functions that use a number, just a number
 function stash-list
-  echo
-  git stash list
-# and some are in here just for consistency
+    echo
+git stash list
+    # and some are in here just for consistency
 end
 function stash-show -a num
-  echo
-  echo Here is the patch from stash@\{$num\}\:
-  git stash show -p stash@\{$num\}
-  echo
+    echo
+    echo Here is the patch from stash@\{$num\}\:
+    git stash show -p stash@\{$num\}
+    echo
 end
 function stash-apply -a num
-  echo
-  echo We are applying stash@\{$num\}
-  stash-show $num
-  echo
-  git stash pop stash@\{$num\}
+    echo
+    echo We are applying stash@\{$num\}
+    stash-show $num
+    echo
+    git stash pop stash@\{$num\}
 end
 function stash-drop --argument num
-  echo
-  echo We are dropping stash@\{$num\}
-  git stash drop stash@\{$num\}
+    echo
+    echo We are dropping stash@\{$num\}
+    git stash drop stash@\{$num\}
 end
 function stash-diff-to -a num
-  # TODO: this is either broken or doesn't look like I thought it would
-  echo
-  echo Here is the diff from your HEAD to stash@\{$num\}\:
-  git diff HEAD stash@\{$num\}
+    # TODO: this is either broken or doesn't look like I thought it would
+    echo
+    echo Here is the diff from your HEAD to stash@\{$num\}\:
+    git diff HEAD stash@\{$num\}
 end
 
 #alias amend="git commit --amend -m (lastlog)"
 function amend
-  set -l my_name (git config user.name)
-  set -l my_email (git config user.email)
+    set -l my_name (git config user.name)
+    set -l my_email (git config user.email)
 
-  set -l previous_author (git log -n 1 --format="%aN %aE")
-  if test "$previous_author" != "$my_name $my_email"
-    set_color red --bold
-    echo "Take a break... You just tried to amend a commit by" (git log -n 1 --format=%aN) '.'
-    set_color normal
-  else
-    set -l lastcommit (git log -n 1 --format="%H")
-    git commit --amend --reuse-message=$lastcommit
-  end
+    set -l previous_author (git log -n 1 --format="%aN %aE")
+    if test "$previous_author" != "$my_name $my_email"
+        set_color red --bold
+        echo "Take a break... You just tried to amend a commit by" (git log -n 1 --format=%aN) '.'
+        set_color normal
+    else
+        set -l lastcommit (git log -n 1 --format="%H")
+        git commit --amend --reuse-message=$lastcommit
+    end
 end
 
 
@@ -125,31 +125,31 @@ set -g async_prompt_functions _pure_prompt_git
 
 # OS Specific Inclusions
 switch (uname)
-case Linux
-case FreeBSD NetBSD DragonFly
-case Darwin
+    case Linux
+    case FreeBSD NetBSD DragonFly
+    case Darwin
 
-  # homebrew openjdk
-  set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+    # homebrew openjdk
+    set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
 
-  # homebrew shell completion
-  if test -d (brew --prefix)"/share/fish/completions"
-      set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-  end
-  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-      set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish vendor_completions.d
-  end
+    # homebrew shell completion
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish vendor_completions.d
+    end
 
-  # perl
-  set -x PATH /Users/alice/perl5/bin $PATH ^/dev/null;
-  set -q PERL5LIB; and set -x PERL5LIB /Users/alice/perl5/lib/perl5:$PERL5LIB;
-  set -q PERL5LIB; or set -x PERL5LIB /Users/alice/perl5/lib/perl5;
-  set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /Users/alice/perl5:$PERL_LOCAL_LIB_ROOT;
-  set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /Users/alice/perl5;
-  set -x PERL_MB_OPT --install_base\ \"/Users/alice/perl5\";
-  set -x PERL_MM_OPT INSTALL_BASE=/Users/alice/perl5;
+    # perl
+    set -x PATH /Users/alice/perl5/bin $PATH ^/dev/null;
+    set -q PERL5LIB; and set -x PERL5LIB /Users/alice/perl5/lib/perl5:$PERL5LIB;
+    set -q PERL5LIB; or set -x PERL5LIB /Users/alice/perl5/lib/perl5;
+    set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /Users/alice/perl5:$PERL_LOCAL_LIB_ROOT;
+    set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /Users/alice/perl5;
+    set -x PERL_MB_OPT --install_base\ \"/Users/alice/perl5\";
+    set -x PERL_MM_OPT INSTALL_BASE=/Users/alice/perl5;
 
-case '*'
+    case '*'
 end
 
 if tput cols > 74 
